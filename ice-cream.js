b2e18b017402aecdc75fe32384df2ab841b1c76e
Erika Yoshida <erikayoshida5@gmail.com>
2021-09-26 17:18:34 -0400

    Added ice-cream challenge using Array

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+const iceCreamFlavours2 = ["root bear"];
+console.log(iceCreamFlavours.concat(iceCreamFlavours2));
+
+console.log(iceCreamFlavours[0]);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours[5]);
+
+console.log(iceCreamFlavours.length);
