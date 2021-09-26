a06adb995d984dfa958072308150574118dd9e12
Erika Yoshida <erikayoshida5@gmail.com>
2021-09-26 14:49:12 -0400

    added function return practice

--- hello.js
+++ hello.js
@@ -2,3 +2,9 @@ const sayHello = function (name){
   console.log("Hello, " + name);
 }
 sayHello("Erika");
+
+const returnSayHello = function (name2){
+  return "Hello, " + name2;
+}
+const greeting = returnSayHello("Erika");
+console.log(greeting);
